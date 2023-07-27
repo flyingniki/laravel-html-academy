3be0944079b603dae597a1dbc2eb96818ee1d7ed
Petr Nikitin <flying_niki@mail.ru>
2023-07-27 13:05:45 +0300

    created laraproject

---
+++ app/Http/Responses/Fail.php
@@ -0,0 +1,34 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Responses\Base;
+
+class Fail extends Base
+{
+    public int $statusCode = Response::HTTP_BAD_REQUEST;
+
+    /**
+     * ExceptionResponse constructor.
+     *
+     * @param $data
+     * @param string|null $message
+     * @param int $code
+     */
+    public function __construct($data, protected ?string $message = null, int $code = Response::HTTP_BAD_REQUEST)
+    {
+        parent::__construct([], $code);
+    }
+
+    /**
+     * Формирование содежимого ответа.
+     *
+     * @return array
+     */
+    protected function makeResponseData(): array
+    {
+        return [
+            'message' => $this->message,
+            'errors' => $this->prepareData(),
+        ];
+    }
+}
