cbd2d29ef06e90f450fb38d37b868bfb1c47cf07
Petr Nikitin <flying_niki@mail.ru>
2023-10-11 10:23:19 +0300

    added model to factories

--- database/factories/FilmFactory.php
+++ database/factories/FilmFactory.php
@@ -10,6 +10,14 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class FilmFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Film::class;
+
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
--- database/factories/GenreFactory.php
+++ database/factories/GenreFactory.php
@@ -10,6 +10,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class GenreFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Genre::class;
+
+    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
